a6a97eecddfbe810fb5171feb156c54b07ba3bec
kouomeu-k <kkouomeu@maisonsdumonde.com>
2023-09-08 22:32:42 +0200

    calculate winner

--- src/app/game/Board.js
+++ src/app/game/Board.js
@@ -1,17 +1,29 @@
 import Square from "./Square";
 import {useState} from "react";
 
-function Board() {
+export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
-    function handleClick(i){
-        const nextSquare  = squares.slice();
-        nextSquare[i] = 'X';
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
+        const nextSquare = squares.slice();
+        nextSquare[i] = xIsNext ? 'X' : 'O';
         setSquares(nextSquare);
-    };
+        setXIsNext(!xIsNext)
+    }
+
+    const winner = calculateWinner(squares);
+    let gameStatus;
+    if (winner) {
+        gameStatus = 'Winner: ' + winner;
+    } else {
+        gameStatus = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
     return (
         <>
+            <div className="status">{gameStatus}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -31,4 +43,23 @@ function Board() {
     )
 }
 
-export default Board;
+function calculateWinner(squares) {
+    const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningLines.length; i++) {
+        const [a, b, c] = winningLines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
